b56657b390b4a927f4ddd210675690f1bab675ca
David Kent <davidkent@wpi.edu>
2012-12-31 19:37:44 -0500

    Input Reader for text file input and formatting

---
+++ PseudoSpeech/src/Input/InputReader.java
@@ -0,0 +1,65 @@
+package Input;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class InputReader {
+	String input;
+	
+	public InputReader(String filename)
+	{
+		StringBuilder text = new StringBuilder();
+		String NL = System.getProperty("line.separator");
+		Scanner scanner;
+		try {
+			scanner = new Scanner(new FileInputStream(filename));
+			try {
+				while (scanner.hasNextLine()) {
+					text.append(scanner.nextLine() + " ");
+				}
+			} finally {
+				scanner.close();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found...");
+			e.printStackTrace();
+		}
+		
+		input = text.toString();
+	}
+	
+	public void formatInput()
+	{
+		input = input.toLowerCase();
+		char c;
+		StringBuilder temp = new StringBuilder(input);
+		for (int i = input.length() - 1; i >= 0; i --)
+		{
+			c = input.charAt(i);
+			if (c < 97 || c > 122)
+			{
+				if (c != ' ' && c != '\'')
+					temp.setCharAt(i, ' ');
+			}
+		}
+		input = temp.toString();
+	}
+	
+	public void removeExcessSpace()
+	{
+		input = input.replaceAll(" +", " ");
+	}
+	
+	public String getInput()
+	{
+		return input;
+	}
+	
+	public static void main(String[] args)
+	{
+		InputReader ir = new InputReader("text/test.txt");
+		ir.formatInput();
+		ir.removeExcessSpace();
+	}
+}
